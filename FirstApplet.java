bf2eecd99efcbbd5450ecff3b191ca5042d87adb
Raju Sarkar <75768853+lobby58@users.noreply.github.com>
2023-05-19 21:11:11 +0530

    Add files via upload

---
+++ FirstApplet.java
@@ -0,0 +1,11 @@
+import java.applet.*;
+import java.awt.*;
+public class FirstApplet extends Applet
+{
+
+    public void paint(Graphics g)
+    {
+        g.drawString("Hello Raju",150,150);
+    }
+    
+}
